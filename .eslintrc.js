efda413a9de30e0bd27041887e8e01e4240b1504
msaoud1337 <aminox1998@outlook.fr>
2024-10-06 18:37:09 +0100

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,10 @@ module.exports = {
 	},
 	rules: {
 		'react/react-in-jsx-scope': 'off',
+		'@typescript-eslint/no-require-imports': 'off',
 		'no-unused-vars': 'warn',
+		'react/no-unescaped-entities': 'off',
 		'react/prop-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'error', // Disallow usage of any
-		'@typescript-eslint/explicit-module-boundary-types': 'error', // Ensure types are explicitly declared
 	},
 };
