fd2a0f597c89a9e811564cbf1696b65fc9e247ae
msaoud1337 <aminox1998@outlook.fr>
2024-10-07 18:24:00 +0100

    feat(swip): add panGestureHandle that prive swip left and right events

--- components/stage/Stage.tsx
+++ components/stage/Stage.tsx
@@ -1,48 +1,72 @@
-import { STAGE } from '@/hooks/useStage';
+import { createStage, useStage } from '@/hooks/useStage';
 import { TETROMINOS } from '@/utils/setup';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+	HandlerStateChangeEvent,
+	PanGestureHandler,
+	PanGestureHandlerEventPayload,
+} from 'react-native-gesture-handler';
+import { usePlayer } from '@/hooks/usePlayer';
 
-export const createStage = () =>
-	Array.from(Array(20), () => Array(12).fill({ value: 0, isClear: true }));
+const { width, height } = Dimensions.get('window');
 
-export const GameStage = ({ stage }: { stage: STAGE }) => {
-	const { width, height } = Dimensions.get('window');
+const gridWidth = width * 0.95;
+const gridHeight = height * 0.9;
 
-	const gridWidth = width * 0.95;
-	const gridHeight = height * 0.9;
+const squareSize = Math.min(gridWidth / 12, gridHeight / 20);
 
-	const squareSize = Math.min(gridWidth / 12, gridHeight / 20);
+const adjustedGridWidth = squareSize * 12;
+const adjustedGridHeight = squareSize * 20;
 
-	const adjustedGridWidth = squareSize * 12;
-	const adjustedGridHeight = squareSize * 20;
+export const GameStage = () => {
+	const { player, resetPlayer, updatePlayerPos } = usePlayer();
+	const { stage, setStage } = useStage(player);
+
+	const onHandlerStateChange = (e: HandlerStateChangeEvent<PanGestureHandlerEventPayload>) => {
+		if (e.nativeEvent.translationX > 100) {
+			updatePlayerPos({ x: 1, y: 0 });
+		} else if (e.nativeEvent.translationX < -100) {
+			updatePlayerPos({ x: -1, y: 0 });
+		}
+	};
+
+	useEffect(() => {
+		setStage(createStage());
+		resetPlayer();
+	}, []);
 
 	return (
 		<SafeAreaView style={styles.safeEreaContainer}>
-			<View
-				style={[styles.container, { width: adjustedGridWidth, height: adjustedGridHeight }]}
-			>
-				{stage?.map((row) =>
-					row.map((cell, index) => {
-						const backgroundColor =
-							TETROMINOS[cell.value as keyof typeof TETROMINOS]?.color;
-						return (
-							<View
-								key={index}
-								style={[
-									styles.square,
-									{
-										width: squareSize - 0.2,
-										height: squareSize,
-										backgroundColor,
-									},
-								]}
-							/>
-						);
-					}),
-				)}
-			</View>
+			<PanGestureHandler onHandlerStateChange={onHandlerStateChange}>
+				<View
+					style={[
+						styles.container,
+						{ width: adjustedGridWidth, height: adjustedGridHeight },
+					]}
+				>
+					{stage?.map((row) =>
+						row.map((cell, index) => {
+							const backgroundColor =
+								TETROMINOS[cell.value as keyof typeof TETROMINOS]?.color;
+							return (
+								<View
+									key={index}
+									style={[
+										styles.square,
+										{
+											width: squareSize - 0.2,
+											height: squareSize,
+											backgroundColor,
+										},
+									]}
+								/>
+							);
+						}),
+					)}
+				</View>
+			</PanGestureHandler>
 		</SafeAreaView>
 	);
 };
--- hooks/useStage.tsx
+++ hooks/useStage.tsx
@@ -1,14 +1,17 @@
-import { createStage } from '@/components/stage/Stage';
 import { useEffect, useState } from 'react';
 import { PLAYER } from './usePlayer';
 
 export type STAGECELL = { value: number | string; isClear: boolean };
 export type STAGE = STAGECELL[][];
 
+export const createStage = () =>
+	Array.from(Array(20), () => Array(12).fill({ value: 0, isClear: true }));
+
 export const useStage = (player?: PLAYER) => {
 	const [stage, setStage] = useState<STAGE>(createStage());
 
 	useEffect(() => {
+		if (!player?.pos?.x) return;
 		const updatedStage = (prevStage: STAGE) => {
 			const newStage = prevStage.map((line) =>
 				line.map((cell) => (cell.isClear ? { value: 0, isClear: true } : cell)),
