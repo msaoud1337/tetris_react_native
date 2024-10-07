31a2c38e410ac37246abe66c16602c85f554277d
msaoud1337 <aminox1998@outlook.fr>
2024-10-07 11:24:22 +0100

    feat(Stage): display tetris Stage

---
+++ components/stage/Stage.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const TetrisGame = () => {
+	const { width, height } = Dimensions.get('window');
+
+	const gridWidth = width * 0.95;
+	const gridHeight = height * 0.9;
+
+	const squareSize = Math.min(gridWidth / 12, gridHeight / 20);
+
+	const adjustedGridWidth = squareSize * 12;
+	const adjustedGridHeight = squareSize * 20;
+
+	return (
+		<SafeAreaView style={styles.safeEreaContainer}>
+			<View
+				style={[styles.container, { width: adjustedGridWidth, height: adjustedGridHeight }]}
+			>
+				{Array.from({ length: 12 * 20 }).map((_, index) => (
+					<View
+						key={index}
+						style={[styles.square, { width: squareSize - 0.2, height: squareSize }]}
+					/>
+				))}
+			</View>
+		</SafeAreaView>
+	);
+};
+
+const styles = StyleSheet.create({
+	safeEreaContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	container: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		borderWidth: 1,
+		borderColor: '#000',
+	},
+	square: {
+		backgroundColor: '#ccc',
+		borderWidth: 1,
+		borderColor: '#000',
+	},
+});
+
+export default TetrisGame;
