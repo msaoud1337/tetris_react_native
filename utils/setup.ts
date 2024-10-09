ac00b0b412da82097df580adfe41cc1f93b44b17
msaoud1337 <aminox1998@outlook.fr>
2024-10-09 12:45:01 +0100

    style(player): add new colors

--- utils/setup.ts
+++ utils/setup.ts
@@ -1,11 +1,13 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
+export const COLORS = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FFA500', '#800080', '#00FFFF'];
+
 export const TETROMINOS = {
 	'0': { shape: [[0]], color: '0, 0, 0' },
 	'.': {
 		shape: [['.']],
-		color: 'red',
+		color: '#FF0000',
 	},
 	I: {
 		shape: [
@@ -14,21 +16,21 @@ export const TETROMINOS = {
 			[0, 'I', 0, 0],
 			[0, 'I', 0, 0],
 		],
-		color: 'red',
+		color: '#00FF00',
 	},
 	U: {
 		shape: [
 			['U', 0, 'U'],
 			['U', 'U', 'U'],
 		],
-		color: 'red',
+		color: '#0000FF',
 	},
 	'7': {
 		shape: [
 			[0, '7', '7'],
 			[0, 0, '7'],
 		],
-		color: 'red',
+		color: '#FFFF00',
 	},
 	J: {
 		shape: [
@@ -36,7 +38,7 @@ export const TETROMINOS = {
 			[0, 'J', 0],
 			['J', 'J', 0],
 		],
-		color: 'red',
+		color: '#FFA500',
 	},
 	L: {
 		shape: [
@@ -44,14 +46,14 @@ export const TETROMINOS = {
 			[0, 'L', 0],
 			[0, 'L', 'L'],
 		],
-		color: 'red',
+		color: '#800080',
 	},
 	O: {
 		shape: [
 			['O', 'O'],
 			['O', 'O'],
 		],
-		color: 'red',
+		color: '#00FFFF',
 	},
 	S: {
 		shape: [
@@ -59,7 +61,7 @@ export const TETROMINOS = {
 			['S', 'S', 0],
 			[0, 0, 0],
 		],
-		color: 'red',
+		color: '#FFA500',
 	},
 	T: {
 		shape: [
@@ -75,6 +77,6 @@ export const TETROMINOS = {
 			[0, 'Z', 'Z'],
 			[0, 0, 0],
 		],
-		color: 'red',
+		color: '#00FF00',
 	},
 };
