7d42348bb33d58eea51786b2815014f40adc40c8
msaoud1337 <aminox1998@outlook.fr>
2024-10-28 12:12:39 +0100

    feat(game): add animated layout on pause/resume game

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,10 @@ import { GameStage } from '@/components/stage/Stage';
 import { useGame } from '@/hooks/useGame';
 import { Feather } from '@expo/vector-icons';
 import { useEffect, useRef, useState } from 'react';
-import { Animated, Pressable, SafeAreaView, View } from 'react-native';
+import { Animated, Button, Dimensions, Pressable, SafeAreaView, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const windowHeight = Dimensions.get('window').height;
 
 const GamePauseStats = ({
 	isGamePaused,
@@ -54,12 +56,22 @@ const GamePauseStats = ({
 export default function HomeScreen() {
 	const [time, setTime] = useState({ minutes: 0, seconds: 0 });
 	const { isGamePlayed, startTheGame, isGamePaused, isGameOver, setGameStats } = useGame();
+	const translateY = useRef(new Animated.Value(windowHeight)).current;
+
+	if (isGamePaused)
+		Animated.timing(translateY, {
+			toValue: (windowHeight / 100) * 35,
+			duration: 500,
+			useNativeDriver: true,
+			delay: 200,
+		}).start();
 
 	useEffect(() => {
 		if (!isGamePlayed) {
 			startTheGame();
 			return;
 		}
+
 		const timer =
 			isGamePlayed &&
 			!isGamePaused &&
@@ -78,20 +90,62 @@ export default function HomeScreen() {
 		return () => clearInterval(Number(timer));
 	}, [isGamePlayed, isGamePaused, isGameOver]);
 
-	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<View style={{ flex: 1, position: 'absolute' }}>
-				<GameHeader minutes={time.minutes} seconds={time.seconds} />
-				<GameBackground />
-			</View>
-			<GestureHandlerRootView>
-				<GamePauseStats
-					isGameOver={isGameOver}
-					isGamePaused={isGamePaused}
-					setGameStats={() => setGameStats(!isGamePaused)}
+	const GameStatsBox = isGamePaused && (
+		<View
+			style={{
+				height: '100%',
+				width: '100%',
+				position: 'absolute',
+				zIndex: 1337,
+				top: 0,
+				left: 0,
+				backgroundColor: 'rgba(0, 0, 0, .5)',
+			}}
+		>
+			<Animated.View
+				style={{
+					transform: [{ translateY }],
+					width: '100%',
+					height: '30%',
+					position: 'absolute',
+					backgroundColor: '#252c93',
+					top: 0,
+					left: 0,
+					zIndex: 1337,
+				}}
+			>
+				<Button
+					title='Press me'
+					onPress={() => {
+						Animated.timing(translateY, {
+							toValue: windowHeight,
+							duration: 500,
+							useNativeDriver: true,
+							delay: 200,
+						}).start(() => setGameStats(false));
+					}}
 				/>
-				<GameStage />
-			</GestureHandlerRootView>
-		</SafeAreaView>
+			</Animated.View>
+		</View>
+	);
+
+	return (
+		<View style={{ flex: 1 }}>
+			<SafeAreaView style={{ flex: 1, position: 'relative' }}>
+				{GameStatsBox}
+				<View style={{ flex: 1, position: 'absolute' }}>
+					<GameHeader minutes={time.minutes} seconds={time.seconds} />
+					<GameBackground />
+				</View>
+				<GestureHandlerRootView>
+					<GamePauseStats
+						isGameOver={isGameOver}
+						isGamePaused={isGamePaused}
+						setGameStats={() => setGameStats(!isGamePaused)}
+					/>
+					<GameStage />
+				</GestureHandlerRootView>
+			</SafeAreaView>
+		</View>
 	);
 }
